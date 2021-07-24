329133bf24adbb26c8ab3eba09882844c4724bf0
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-07-24 20:35:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-python3 -m venv env && source env/bin/activate && chmod +x * && ./setup.sh && screen -S 000 ./START.sh
+apt install python3.8-venv -y && apt install screen -y && python3 -m venv env && source env/bin/activate && chmod +x * && ./setup.sh && screen -S 000 ./START.sh
